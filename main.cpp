ee6ff315d6fd7d99d5e90454dfcf9d56d07f3383
Krystian <250939@edu.p.lodz.pl>
2024-05-09 19:55:07 +0200

    menu

--- main.cpp
+++ main.cpp
@@ -3,7 +3,48 @@
 #include <stdlib.h>
 FILE * fptr;
 
+void menu() {
+    printf("1- Input Values\n");
+    printf("2- Display Array\n");
+    printf("3- Maximum Value\n");
+    printf("4- Minimum Value\n");
+    printf("5- Calculate Average\n");
+    printf("6- Enter into file\n");
+    printf("7- Read file\n");
+    printf("8- Stop Program\n");
+}
+
 int main() {
     int array[SIZE] = {1, 2, 3, 4, 5, 6, 7 ,8, 9, 10};
+    int choice = 0;
+
+    while(choice != 8){
+        menu();
+        printf("Select program: \n");
+        scanf("%d", &choice);
+        switch(choice){
+            case 1:
+
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            case 6:
+
+                break;
+            case 7:
+
+                break;
+        }
+    }
     return 0;
 }
